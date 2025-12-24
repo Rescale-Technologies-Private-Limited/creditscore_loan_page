f46a50830366b694174a4cc9fc7558d9c70be9c8
ranjatrescale <ranjat.fuke@fincash.com>
2025-12-24 18:47:17 +0530

    Fix URL typo in fetch request for OTP generation

--- public/form_script.js
+++ public/form_script.js
@@ -481,7 +481,7 @@ async function validateForm1() {
 
   try {
     const response = await fetch(
-      "ttps://asia-south1-ads-ai-101.cloudfunctions.net/loan_api_1/bajaj/generateOTP",
+      "https://asia-south1-ads-ai-101.cloudfunctions.net/loan_api_1/bajaj/generateOTP",
       // https://asia-south1-ads-ai-101.cloudfunctions.net/card_api/getotp
       // "http://localhost//api/generateOTP",
       {
